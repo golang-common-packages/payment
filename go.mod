af0de8424f6e5cd00bc2e6f895ee305e84e58e71
nbxtruong <omega220@gmail.com>
2020-02-04 15:02:12 +0700

    [PayPal] (*) Change to SDK v3

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.13
 
 require (
 	github.com/plaid/plaid-go v0.0.0-20200203222735-1c732a9e58fa
-	github.com/plutov/paypal v2.0.5+incompatible
+	github.com/plutov/paypal/v3 v3.0.12
 	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/stripe/stripe-go v68.10.0+incompatible
 	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
--- paypal.go
+++ paypal.go
@@ -1,32 +1,70 @@
 package payment
 
 import (
+	"io"
 	"log"
+	"net/http"
 	"os"
 
-	"github.com/plutov/paypal"
+	"github.com/plutov/paypal/v3"
 )
 
+// PaypalClient ...
 type PaypalClient struct {
 	client *paypal.Client
+	token  *paypal.TokenResponse
 }
 
-func NewPaypalClient(clientID, secretID string) *PaypalClient {
-	currentSesstion := &PaypalClient{nil}
+// NewPaypal ...
+func NewPaypal(clientID, secretID string) *PaypalClient {
+	currentSesstion := &PaypalClient{nil, nil}
 
-	client, err := paypal.NewClient("clientID", "secretID", paypal.APIBaseSandBox)
+	client, err := paypal.NewClient("clientID", "secretID", paypal.APIBaseLive)
 	if err != nil {
-		log.Println("Error when try to make strconv port from config: ", err)
+		log.Println("Error when init paypal client: ", err)
 		panic(err)
 	}
 	client.SetLog(os.Stdout) // Set log to terminal stdout
 
+	tokenResult, err := client.GetAccessToken()
+	if err != nil {
+		log.Println("Error when get paypal token: ", err)
+		panic(err)
+	}
+
 	currentSesstion.client = client
+	currentSesstion.token = tokenResult
 
 	return currentSesstion
 }
 
-func (pp *PaypalClient) SubmitPayment(emailSubject, recipientType, receiver, amount, currencyType, sendingNote string) (interface{}, error) {
+// GetAccessTokenFromAuthCode ...
+func (pp *PaypalClient) GetAccessTokenFromAuthCode(code, redirectURL string) (result *paypal.TokenResponse, err error) {
+	return pp.client.GrantNewAccessTokenFromAuthCode(code, redirectURL)
+}
+
+// GetAccessTokenFromRefreshToken ...
+func (pp *PaypalClient) GetAccessTokenFromRefreshToken(refreshToken string) (result *paypal.TokenResponse, err error) {
+	return pp.client.GrantNewAccessTokenFromRefreshToken(refreshToken)
+}
+
+// SetAccessToken ...
+func (pp *PaypalClient) SetAccessToken(accessToken string) {
+	pp.client.SetAccessToken(accessToken)
+}
+
+// SetHTTPClient ...
+func (pp *PaypalClient) SetHTTPClient(httpClient *http.Client) {
+	pp.client.SetHTTPClient(httpClient)
+}
+
+// SetLog ...
+func (pp *PaypalClient) SetLog(log io.Writer) {
+	pp.client.SetLog(log)
+}
+
+// SubmitPayment ...
+func (pp *PaypalClient) SubmitPayment(emailSubject, recipientType, receiver, amount, currencyType, sendingNote string) (result *paypal.PayoutResponse, err error) {
 	payout := paypal.Payout{
 		SenderBatchHeader: &paypal.SenderBatchHeader{
 			EmailSubject: emailSubject,
@@ -52,64 +90,78 @@ func (pp *PaypalClient) SubmitPayment(emailSubject, recipientType, receiver, amo
 	return payoutResp, nil
 }
 
-func (pp *PaypalClient) GetPayment(payoutBatchID string) (interface{}, error) {
-	payloads, err := pp.client.GetPayout(payoutBatchID)
-	if err != nil {
-		return nil, err
-	}
-
-	return payloads, nil
+// ConvertRequestToJSON ...
+func (pp *PaypalClient) ConvertRequestToJSON(method, url string, payload interface{}) (httpClient *http.Request, err error) {
+	return pp.client.NewRequest(method, url, payload)
 }
 
-func (pp *PaypalClient) GetPaymentItem(payoutBatchID string) (interface{}, error) {
-	payload, err := pp.client.GetPayoutItem(payoutBatchID)
-	if err != nil {
-		return nil, err
-	}
-
-	return payload, nil
+// SendRequest ...
+func (pp *PaypalClient) SendRequest(req *http.Request, value interface{}) error {
+	return pp.client.Send(req, value)
 }
 
-func (pp *PaypalClient) ListCreditCards(page, pageSize int) (interface{}, error) {
+// SendRequestWithAuth ...
+func (pp *PaypalClient) SendRequestWithAuth(req *http.Request, value interface{}) error {
+	return pp.client.SendWithAuth(req, value)
+}
+
+// SendRequestWithBaseAuth ...
+func (pp *PaypalClient) SendRequestWithBaseAuth(req *http.Request, value interface{}) error {
+	return pp.client.SendWithBasicAuth(req, value)
+}
+
+// CreateBillingAgreement ...
+func (pp *PaypalClient) CreateBillingAgreement(bill paypal.BillingAgreement) (result *paypal.CreateAgreementResp, err error) {
+	return pp.client.CreateBillingAgreement(bill)
+}
+
+// CreateBillingPlan ...
+func (pp *PaypalClient) CreateBillingPlan(bill paypal.BillingPlan) (result *paypal.CreateBillingResp, err error) {
+	return pp.client.CreateBillingPlan(bill)
+}
+
+// GetPayment ...
+func (pp *PaypalClient) GetPayment(payoutBatchID string) (result *paypal.PayoutResponse, err error) {
+	return pp.client.GetPayout(payoutBatchID)
+}
+
+// GetPaymentItem ...
+func (pp *PaypalClient) GetPaymentItem(payoutBatchID string) (result *paypal.PayoutItemResponse, err error) {
+	return pp.client.GetPayoutItem(payoutBatchID)
+}
+
+// ListCreditCards ...
+func (pp *PaypalClient) ListCreditCards(page, pageSize int) (result *paypal.CreditCards, err error) {
 	creditCardFilter := &paypal.CreditCardsFilter{
 		PageSize: pageSize,
 		Page:     page,
 	}
 
-	creaditCards, err := pp.client.GetCreditCards(creditCardFilter)
-	if err != nil {
-		return nil, err
-	}
-
-	return creaditCards, nil
+	return pp.client.GetCreditCards(creditCardFilter)
 }
 
-func (pp *PaypalClient) GetCreditCardDetail(creditCardID string) (interface{}, error) {
-	detail, err := pp.client.GetCreditCard(creditCardID)
-	if err != nil {
-		return nil, err
-	}
-
-	return detail, nil
+// GetCreditCardDetail ...
+func (pp *PaypalClient) GetCreditCardDetail(creditCardID string) (result *paypal.CreditCard, err error) {
+	return pp.client.GetCreditCard(creditCardID)
 }
 
-func (pp *PaypalClient) StoreCreditCardDetail(line1, line2, city, countryCode, postalCode, state, phone, id, payerID, externalCustomerID, number, typeCard, expireMonth, expireYear, cvv2, firstName, lastName, State, ValidUntil string) (interface{}, error) {
+// StoreCreditCardDetail ...
+func (pp *PaypalClient) StoreCreditCardDetail(line1, line2, city, countryCode, postalCode, state, phone, id, payerID, externalCustomerID, number, typeCard, expireMonth, expireYear, cvv2, firstName, lastName, State, ValidUntil string) (result *paypal.CreditCard, err error) {
 	billingAddress := generateAddress(line1, line2, city, countryCode, postalCode, state, phone)
 	creditCard := generateCreditDetail(id, payerID, externalCustomerID, number, typeCard, expireMonth, expireYear, cvv2, firstName, lastName, State, ValidUntil, billingAddress)
 
-	result, err := pp.client.StoreCreditCard(creditCard)
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
+	return pp.client.StoreCreditCard(creditCard)
 }
 
+// RemoveCreditCardDetail ...
 func (pp *PaypalClient) RemoveCreditCardDetail(creditCardID string) error {
 	err := pp.client.DeleteCreditCard(creditCardID)
 	return err
 }
 
 // Paypal util
+
+// generateAddress ...
 func generateAddress(line1, line2, city, countryCode, postalCode, state, phone string) *paypal.Address {
 	address := &paypal.Address{
 		Line1:       line1,
@@ -124,6 +176,7 @@ func generateAddress(line1, line2, city, countryCode, postalCode, state, phone s
 	return address
 }
 
+// generateCreditDetail ...
 func generateCreditDetail(id, payerID, externalCustomerID, number, typeCard, expireMonth, expireYear, cvv2, firstName, lastName, State, ValidUntil string, billingAddress *paypal.Address) paypal.CreditCard {
 	creditCardDetail := paypal.CreditCard{
 		ID:                 id,
