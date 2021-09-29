fe05c1759cc449b75d31ac983ae6c12bf3719965
nbxtruong <omega220@gmail.com>
2021-09-29 16:37:26 +0700

    (-) Remove unused methods

--- util.go
+++ util.go
@@ -1,11 +1,7 @@
 package payment
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"hash/fnv"
-	"io"
 )
 
 // SetContext set new context
@@ -16,23 +12,4 @@ func SetContext(context context.Context) {
 // GetContext return the current context
 func GetContext() context.Context {
 	return ctx
-}
-
-func generateKey(data string) string {
-	hash := fnv.New64a()
-	hash.Write([]byte(data))
-
-	return fmt.Sprint(hash.Sum64())
-}
-
-func streamToByte(stream io.Reader) []byte {
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(stream)
-	return buf.Bytes()
-}
-
-func streamToString(stream io.Reader) string {
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(stream)
-	return buf.String()
 }
